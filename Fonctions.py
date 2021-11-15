015e011a314fdd3772dc3ffb929ae8f695db0dbc
Asma-CHER <cheras99@gmail.com>
2021-11-16 00:49:31 +0100

    Initial commit

---
+++ Fonctions.py
@@ -0,0 +1,42 @@
+import cv2
+from textwrap import wrap
+
+''' Fonctions '''
+def dec_bin(table):
+    msgBinaire = ""
+    h, w = table.shape
+    for y in range(h):
+        for x in range(w):
+            t= (int)(bin(table[y,x])[2:])
+            #print(t)
+            val = t % 10
+            #print(val)
+            if(val==0):
+                msgBinaire +=str(1)
+            else:
+                msgBinaire +=str(0)
+
+    #print(msgBinaire)
+    return msgBinaire
+
+
+
+#fonction de decodage du binaire vers du text (code ascii)
+def getASCII(val_dec):
+
+    pass
+
+
+def dec_ASCII(msgBinary):
+    msgText = ""
+    k = wrap(msgBinary, 8)
+    #print(k)
+
+    for b in k:
+        val_dec = int(b, 2)
+        msgText += chr(val_dec)
+        print(val_dec," = ",chr(val_dec))
+
+
+    print(" end ",msgText)
+    return msgText
---
+++ Main.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import Fonctions
+
+img = cv2.imread('image.jpg',cv2.IMREAD_GRAYSCALE)
+
+if img is None:
+    print('image vide')
+else :
+    msgBinary = Fonctions.dec_bin(img) #je lui donne l'image recu elle me donne le msg en binaire avec decodage (inversement de la valeur)
+    msgText = Fonctions.dec_ASCII(msgBinary)
+
+
