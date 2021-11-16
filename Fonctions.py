b887f2a962a98eea1860a5a8a12d7c0172c9a4e8
racha <mekkiracha@gmail.com>
2021-11-17 00:13:11 +0100

    codage

--- Fonctions.py
+++ Fonctions.py
@@ -1,3 +1,6 @@
+import os
+import random
+
 import cv2
 from typing import Iterable
 import numpy as np
@@ -5,26 +8,27 @@ from PIL import Image
 from textwrap import wrap
 
 ''' Fonctions '''
+
+
 def dec_bin(table):
     msgBinaire = ""
     h, w = table.shape
     for y in range(h):
         for x in range(w):
-            t= (int)(bin(table[y,x])[2:])
+            t = (int)(bin(table[y, x])[2:])
             #print(t)
             val = t % 10
             #print(val)
-            if(val==0):
-                msgBinaire +=str(1)
+            if (val == 0):
+                msgBinaire += str(1)
             else:
-                msgBinaire +=str(0)
+                msgBinaire += str(0)
 
-    #print(msgBinaire)
+    # print(msgBinaire)
     return msgBinaire
 
 
-
-#fonction de decodage du binaire vers du text (code ascii)
+# fonction de decodage du binaire vers du text (code ascii)
 def dec_ASCII(msgBinary):
     msgText = ""
     k = wrap(msgBinary, 8)
@@ -35,28 +39,49 @@ def dec_ASCII(msgBinary):
     return msgText
 
 
-def bits_provider(message) -> Iterable[int]:
-    for char in message:
-        ascii_value = ord(char)
-        for bit_position in range(8):
-            power = 7 - bit_position
-            yield 1 if ascii_value & (1 << power) else 0
-
-def create_image(message: str, input, output: str) -> None:
-    img = Image.open(input)
-    pixels = np.array(img)
-    img.close()
-    clear_low_order_bits(pixels)
-    for i, bit in enumerate(bits_provider(message)):
-        row = i // pixels.shape[1]
-        col = i % pixels.shape[1]
-        pixels[row, col,0] |= bit
-    out_img = Image.fromarray(pixels)
-    out_img.save(output)
-    out_img.close()
+#def to_bit_generator(msg):
+#    for c in (msg):
+#        o = ord(c)  #unicode
+#        for i in range(8):
+#            yield (o & (1 << i)) >> i
+def to_bit_generator(msg):
+    result = []
+    for c in msg:
+        bits = bin(ord(c))[2:]
+        bits = '00000000'[len(bits):] + bits
+        result.extend([int(b) for b in bits])
+    return result
 
 
-def clear_low_order_bits(pixels) -> None:
-    for row in range(pixels.shape[0]):
-        for col in range(pixels.shape[1]):
-            pixels[row, col,0] &= ~1
+def code_image (message, img, output :str) :
+    hidden_message = to_bit_generator(message)
+    #print (hidden_message)
+    # Read the original image
+    image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    h, w = image.shape
+    #length = len (message) *8
+    #key = []
+    count =0
+    for y in range (h):
+        for x in range (w):
+            r = (bin (image [y,x] ))[2:]
+            list = wrap(r, 1)
+                #print (k)
+                #print(r)
+            k = hidden_message[count]
+            if k == '0':
+                list [-1] = '1'
+            else:
+                list[-1] = '0'
+            m = ''.join(list)
+            l = int(m,2)  #decimale
+            image[y,x] = l
+            count =+1
+        #position = (-1,-1)
+        #while position not in key:
+        #    position = (random.randint(0, h), random.randint(0,w))
+        #key.append (position)
+
+    # Write out the image with hidden message
+    cv2.imwrite(output, image)
+    return image
--- Main.py
+++ Main.py
@@ -1,19 +1,11 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+
 import Fonctions
 
-img = cv2.imread('img.png',cv2.IMREAD_GRAYSCALE)
-
-if img is None:
-    print('image vide')
-else :
-    message = "hello usthb"
-    Fonctions.create_image(message, 'img.png', 'secret-image.png')
-    imgsec = cv2.imread('secret-image.png', cv2.IMREAD_GRAYSCALE)
-    msgBinary = Fonctions.dec_bin(imgsec) #je lui donne l'image recu elle me donne le msg en binaire avec decodage (inversement de la valeur)
-    msgText = Fonctions.dec_ASCII(msgBinary)
-    print(" end ",msgText)
+message = "hell"
+imgsec = Fonctions.code_image(message, 'image.jpg','imgsec.jpg')
+msgBinary = Fonctions.dec_bin(imgsec) #je lui donne l'image recu elle me donne le msg en binaire avec decodage (inversement de la valeur)
+msgText = Fonctions.dec_ASCII(msgBinary)
+print(" end ",msgText)
 
 
 
