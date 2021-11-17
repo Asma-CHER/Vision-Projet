44c4f1e33f0cf722d56441b795f26b0b29f9bf55
Asma-CHER <cheras99@gmail.com>
2021-11-17 01:46:47 +0100

    +

--- Fonctions.py
+++ Fonctions.py
@@ -11,11 +11,15 @@ from textwrap import wrap
 
 
 def dec_bin(table):
+    print("heere2", table[0, 7])
+
     msgBinaire = ""
     h, w = table.shape
+    #print(h," ",w)
     for y in range(h):
         for x in range(w):
             t = (int)(bin(table[y, x])[2:])
+
             #print(t)
             val = t % 10
             #print(val)
@@ -24,7 +28,7 @@ def dec_bin(table):
             else:
                 msgBinaire += str(0)
 
-    # print(msgBinaire)
+    #print(msgBinaire)
     return msgBinaire
 
 
@@ -36,7 +40,11 @@ def dec_ASCII(msgBinary):
 
     for b in k:
         #print(int(b, 2))
-        msgText += chr(int(b, 2))
+        n = int(b, 2)
+        if n<=172 :
+            msgText += chr(n)
+        else:
+            break
 
     return msgText
 
@@ -51,39 +59,54 @@ def to_bit_generator(msg):
     for c in msg:
         bits = bin(ord(c))[2:]
         bits = '00000000'[len(bits):] + bits
+        #print(bits)
         result.extend([int(b) for b in bits])
+        #print(result)
     return result
 
 
 def code_image (message, img, output :str) :
     hidden_message = to_bit_generator(message)
-    #print (hidden_message)
-    # Read the original image
+    #print(hidden_message)
+
+
     image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     h, w = image.shape
-    #length = len (message) *8
-    #key = []
+
     count =0
     for y in range (h):
         for x in range (w):
             r = (bin (image [y,x] ))[2:]
             list = wrap(r, 1)
-                #print (k)
-                #print(r)
-            k = hidden_message[count]
-            if k == '0':
-                list [-1] = '1'
+            #print (k)
+            #print(r)
+            if count < len(hidden_message):
+                k = hidden_message[count]
+                count+=1
             else:
+                break
+            #print(k)
+            if int(k) == 0:
+                #print("k=0",k)
+                list [-1] = '1'
+            elif int(k) == 1:
+                #print("k=1",k)
                 list[-1] = '0'
             m = ''.join(list)
-            l = int(m,2)  #decimale
+            #print(m)
+
+
+            l = int(m,2)
+            #print(l) #decimale
+
             image[y,x] = l
-            count =+1
-        #position = (-1,-1)
-        #while position not in key:
-        #    position = (random.randint(0, h), random.randint(0,w))
-        #key.append (position)
+            #print(image[y,x])
+            #count =+1
+
 
     # Write out the image with hidden message
     cv2.imwrite(output, image)
+
+
+
     return image
--- Main.py
+++ Main.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Fonctions
 
-img = cv2.imread('image.jpg',cv2.IMREAD_GRAYSCALE)
+'''
+img = cv2.imread('image.png',cv2.IMREAD_GRAYSCALE)
 
 if img is None:
     print('image vide')
@@ -11,22 +12,28 @@ else :
 
 
     message = "hello usthb"
-    Fonctions.create_image(message, 'image.jpg', 'secret-image.png')
+    Fonctions.create_image(message, 'image.png', 'secret-image.png')
     imgsec = cv2.imread('secret-image.png', cv2.IMREAD_GRAYSCALE)
     msgBinary = Fonctions.dec_bin(imgsec) #je lui donne l'image recu elle me donne le msg en binaire avec decodage (inversement de la valeur)
     msgText = Fonctions.dec_ASCII(msgBinary)
     print(" end ",msgText)
-message = "hell"
-imgsec = Fonctions.code_image(message, 'image.jpg','imgsec.jpg')
+    
+    
+    cv2.imshow(' img before ', img)
+    cv2.imshow(' img code ', imgsec)
+'''
+
+message = "hello usthb racha asma vision artificielle blaaa blaaaaaaaa before after script de programme java python bla blaa         mmmm plus moins rcr2 tp1 tp2 projet data mining pour jeudi"
+imgsec = Fonctions.code_image(message, 'image.png','imgsec.png')
+
+i = cv2.imread('imgsec.png', cv2.IMREAD_GRAYSCALE)
+
+print("heere1",i[0,7])
+
 msgBinary = Fonctions.dec_bin(imgsec) #je lui donne l'image recu elle me donne le msg en binaire avec decodage (inversement de la valeur)
 msgText = Fonctions.dec_ASCII(msgBinary)
 print(" end ",msgText)
 
-    img = cv2.imread("image.jpg", cv2.IMREAD_GRAYSCALE)
-
-
-    cv2.imshow(' img before ', img)
-    cv2.imshow(' img code ', imgsec)
 
 
 
