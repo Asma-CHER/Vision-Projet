695b28724c915a150122d9ab0059a456b51f7131
Asma-CHER <cheras99@gmail.com>
2021-12-03 20:25:39 +0100

    update

--- Partie2/Functions_Etape2.py
+++ Partie2/Functions_Etape2.py
@@ -1,14 +1,47 @@
-
-
+import Functions_Etape1 as f1
+import numpy as np
+from numpy import array
+from numpy.linalg import pinv
 
 '''
 Créer une fonction calcul_needle_map qui calcule le champ de normales (le vecteur normal pour chaque pixel).
 '''
-
 def calcul_needle_map():
     #Charger les N images dans une variable obj_images(à l'aide de la fonction: load_images)
+    print(" LOAD IMAGES  ************************************************************************")
+    obj_images = f1.load_images()
 
     #charger les positions des sources lumineuses dans une variable light_sources(à l'aide de la fonction: load_lightSources
+    print(" LOAD Light_SOURCES   ************************************************************************")
+    lights = f1.load_lightSources()
+
+    #charger le masque de l’objet dans une variable obj_masques(à l’aide de la fonction: load_objMask)
+    print(" LOAD MASK   ************************************************************************")
+    obj_masques = f1.load_objMask()
 
 
-    pass
+def Normale():
+
+    image = f1.load_images()
+    a = f1.load_lightSources()
+    sInv = pinv(a)
+    print("here shape image ", image.shape, " here shape sInv ", sInv.shape)
+
+    mult = np.dot(sInv, image)
+    print("here shape mult ", mult.shape)
+    print("here mult ", mult)
+
+
+    normal = np.ndarray((512, 612,3))
+
+    for y in range(512):  # ligne par ligne
+        for x in range(612):
+            normal[y,x][0] = mult[0,y*x]
+            normal[y,x][1] = mult[1,y*x]
+            normal[y,x][2] = mult[2,y*x]
+
+            # on calcule la norme (longeur)
+            # on normalise les donnees
+
+
+    return normal
--- Partie2/main.py
+++ Partie2/main.py
@@ -1,26 +1,23 @@
 import cv2
 import numpy as np
 import Functions_Etape1 as f1
+import Functions_Etape2 as f2
 
 
 
-# Etape 1. Préparation de données
-print(" LOAD LIGHT_SOURCES  ************************************************************************")
-lights = f1.load_lightSources()
-print(" HERE SHAPE OF LIGHTS ",lights.shape)
-
-print(" LOAD INTENS_SOURCES   ************************************************************************")
-intenses = f1.load_intensSources()
-print(" HERE SHAPE OF intenses",intenses.shape)
-
-print(" LOAD MASK   ************************************************************************")
-mask = f1.load_objMask()
-print(" HERE SHAPE OF mask",mask.shape)
+#f2.calcul_needle_map()
 
 print(" LOAD IMAGES   ************************************************************************")
-#image = f1.load_images(lights)
-#f1.load_N_images()
-f1.load_images()
+
+
+normal = f2.Normale()
+print("here normal apres ",normal)
+print("here normal shape ",normal.shape)
+
+
+
+
+
 
 
 
