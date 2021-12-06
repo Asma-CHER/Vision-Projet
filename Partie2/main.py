561571e8c32f896c5629f5908d3e8c1109862e74
Asma CHER <59102120+Asma-CHER@users.noreply.github.com>
2021-12-06 20:32:22 +0100

    Update main.py

--- Partie2/main.py
+++ Partie2/main.py
@@ -4,22 +4,19 @@ import Functions_Etape1 as f1
 import Functions_Etape2 as f2
 
 
+#m
+normal = f2.calcul_needle_map()
 
-#f2.calcul_needle_map()
+#normal = f2.Normale()
 
-print(" LOAD IMAGES   ************************************************************************")
+#n=normal
+#image = f2.read_file("my_img.pkl")
 
-
-normal = f2.Normale()
-print("here normal apres ",normal)
-print("here normal shape ",normal.shape)
-
-
-
-
-
-
+#m = f2.showImage(n)
+cv2.imshow("image source",normal)
+cv2.imwrite('imageNormale.png', normal)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
-# Etape 2. Calcul des normales
 
