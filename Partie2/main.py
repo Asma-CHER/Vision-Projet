78fd237778d11d876b2fb60b3142f50de485526d
Asma CHER <59102120+Asma-CHER@users.noreply.github.com>
2021-12-07 21:17:09 +0100

    Update main.py

--- Partie2/main.py
+++ Partie2/main.py
@@ -2,9 +2,8 @@ import cv2
 import numpy as np
 import Functions_Etape1 as f1
 import Functions_Etape2 as f2
+import Functions_Etape3 as f3
 
-
-#m
 normal = f2.calcul_needle_map()
 
 #normal = f2.Normale()
@@ -13,8 +12,15 @@ normal = f2.calcul_needle_map()
 #image = f2.read_file("my_img.pkl")
 
 #m = f2.showImage(n)
+
+
+imageZ= f3.z(normal)
+print(imageZ)
+
 cv2.imshow("image source",normal)
-cv2.imwrite('imageNormale.png', normal)
+cv2.imshow("image z",imageZ)
+
+#cv2.imwrite('imageNormale.png', normal)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
