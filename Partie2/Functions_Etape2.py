313f4ca7226967f661f550594623eea9dfd2f261
Asma CHER <59102120+Asma-CHER@users.noreply.github.com>
2021-12-07 21:18:02 +0100

    Update Functions_Etape2.py

--- Partie2/Functions_Etape2.py
+++ Partie2/Functions_Etape2.py
@@ -10,8 +10,8 @@ Créer une fonction calcul_needle_map qui calcule le champ de normales (le vecte
 '''
 def calcul_needle_map():
     #Charger les N images dans une variable obj_images(à l'aide de la fonction: load_images)
-    obj_images = f1.load_images()
-    #obj_images = read_file("normal.pkl")
+    #obj_images = f1.load_images()
+    obj_images = read_file("normal.pkl")
 
     #charger les positions des sources lumineuses dans une variable light_sources(à l'aide de la fonction: load_lightSources
     lights = f1.load_lightSources()
